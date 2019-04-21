01bdf8fa0da0332a2255ad2ae8192294fbfe3c8e
ngardner84 <brytstahr@yahoo.com>
2019-04-21 01:54:42 -0700

    Updated main menu and script to change scenes from play button

---
+++ Learnt/Learnt/Assets/Scripts/PlaySceneChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySceneChange : MonoBehaviour
+{
+    public void OnMouseClick()
+    {
+        Debug.Log("Clicked button");
+        SceneManager.LoadScene("main");
+    }
+}
