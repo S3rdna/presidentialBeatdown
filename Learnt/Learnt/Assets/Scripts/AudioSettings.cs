f297cc15f6ed949a5fa6bac770b567eacc53d342
ngardner84 <brytstahr@yahoo.com>
2019-04-21 06:15:33 -0700

    Created script to update music volume according to slider value in settings

---
+++ Learnt/Learnt/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "AudioSettings", menuName = "Custom/AudioSettings", order = 1)]
+public class AudioSettings : ScriptableObject
+{
+    // Event
+    public Action OnVolumeChanged = delegate { };
+
+    private float _volume = 0.0f;
+    // Property
+    public float volume
+    {
+        get
+        {
+            return _volume;
+        }
+        set
+        {
+            if(value != _volume)
+            {
+                _volume = value;
+                OnVolumeChanged();
+            }
+            _volume = value;
+        }
+    }
+}
---
+++ Learnt/Learnt/Assets/VolumeControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    private AudioSource myAudioSource;
+    public AudioSettings audioSettings;
+
+    private void OnEnable()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+        UpdateVolume();
+
+        // Subscribe to event.
+        audioSettings.OnVolumeChanged += UpdateVolume;
+    }
+    private void OnDisable()
+    {
+        // Unsubscribe to event.
+        audioSettings.OnVolumeChanged += UpdateVolume;
+    }
+
+    private void UpdateVolume()
+    {
+        myAudioSource.volume = audioSettings.volume;
+    }
+}
