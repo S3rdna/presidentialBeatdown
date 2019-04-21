09d359b26d52cb769e8cd9390ef02aa57f553e1d
ngardner84 <brytstahr@yahoo.com>
2019-04-21 02:05:56 -0700

    Added more main menu buttons and scripts

---
+++ Learnt/Learnt/Assets/Scripts/SettingsSceneChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsSceneChange : MonoBehaviour
+{
+    public void SettingsClick()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
