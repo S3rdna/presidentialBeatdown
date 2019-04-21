fcd34deaaab3ee218941f6c97eb7950f494b2e68
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2019-04-21 06:36:41 -0700

    Animation Finished

--- Learnt/Learnt/Assets/Scripts/PlayerMovement.cs
+++ Learnt/Learnt/Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,54 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 4f;
-    public float jumpForce = 10f;
+    public float jumpForce = 6f;
+    public bool direction = false;
+
+    public Animator anim;
+
+    void Start()
+    {
+        if(anim == null)
+        {
+            anim = gameObject.GetComponent<Animator>();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-
         Vector3 currentPosition = transform.position;
-
         float deltaX = (horizontalInput * speed * Time.deltaTime);
-
         transform.position = new Vector3(
             currentPosition.x + deltaX,
             currentPosition.y,
             currentPosition.z);
 
-        if (Input.GetKey(KeyCode.S) )
+        anim.SetFloat("Hspeed", Mathf.Abs(horizontalInput));
+        if(Mathf.Abs(horizontalInput)>0)
+        {
+            if (horizontalInput > 0)
+            {
+                //Debug.Log("Leftwards");
+                transform.localScale = new Vector3(-1, 1, 1);
+                //transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if(horizontalInput < 0)
+            {
+                //Debug.Log("Rightwards");
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
         {
             Debug.Log("FALL!");
             Rigidbody2D myRigidbody = gameObject.GetComponent<Rigidbody2D>();
             if (myRigidbody != null)
             {
-                myRigidbody.AddForce(new Vector2(0, -jumpForce), ForceMode2D.Impulse);
+                myRigidbody.AddForce(new Vector2(0, -jumpForce * .045f), ForceMode2D.Impulse);
             }
         }
 
@@ -37,35 +63,64 @@ public class PlayerMovement : MonoBehaviour
             if (myRigidbody != null)
             {
                 myRigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("PUNCH!");
-            Animator anim = gameObject.GetComponent<Animator>();
             if (anim != null)
             {
-                anim.SetTrigger("Punch");
+                anim.SetTrigger("PunchRight");
             }
+            
+
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("Right!");
-            Animator anim = gameObject.GetComponent<Animator>();
-            if (anim != null)
-            {
-                anim.SetTrigger("Right");
-            }
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("LEFT!");
-            Animator anim = gameObject.GetComponent<Animator>();
-            if (anim != null)
-            {
-                anim.SetTrigger("Left");
-            }
-        }
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    if (anim != null)
+        //    {
+        //        anim.SetTrigger("WalkRight");
+        //    }
+        //    // direction = false; //false = right
+        //}
+
+        //if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Space))
+        //{
+        //    if (anim != null)
+        //    {
+        //        anim.SetTrigger("WalkRight");
+        //    }
+        //    //direction = false;
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Debug.Log("LEFT!");
+        //    if (anim != null)
+        //    {
+        //        anim.SetTrigger("WalkLeft");
+        //    }
+        //    //direction = true; //true = left
+        //}
+
+        //    if (!direction)
+        //    {
+        //        if (anim != null)
+        //        {
+        //            anim.SetTrigger("Idle");
+        //        }
+        //    }
+
+        //    if (direction)
+        //    {
+        //        if (anim != null)
+        //        {
+        //            anim.SetTrigger("IdleLeft");
+        //        }
+        //    }
+        //}
     }
 }
